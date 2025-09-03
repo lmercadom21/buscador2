fe319ebf44a2d7e499fbf228a1221821c5bcec80
lmercadom21 <lmercadom21@campusuninunez.edu.co>
2025-09-02 19:47:47 -0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,10 +1,36 @@
-from http.server import BaseHTTPRequestHandler
+import tkinter as tk
 
-class handler(BaseHTTPRequestHandler):
 
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/plain')
-        self.end_headers()
-        self.wfile.write('Hello, world!'.encode('utf-8'))
-        return
+palabras = [
+    "moto nkd","moto nkd 125","moto nkd 150"
+]
+
+def sugerenciass(event):
+    texto = buscar.get().lower()
+    lista_sugerencias.delete(0, tk.END)
+    
+    if texto:  
+        sugerencias = [p for p in palabras if texto in p.lower()]
+        for sugerencia in sugerencias[:3]:  
+            lista_sugerencias.insert(tk.END, sugerencia)
+
+
+
+ventana= tk.Tk()
+ventana.title=("Buscador")
+ventana.geometry("250x250")
+
+buscar = tk.Entry()
+buscar.pack()
+buscar.bind("<KeyRelease>", sugerenciass)
+
+
+lista_sugerencias= tk.Listbox(ventana)
+lista_sugerencias.pack()
+
+ventana.mainloop()
+
+#Referencias :
+# https://docs.python.org/es/3.13/library/tkinter.html
+#https://pythonguides.com/python-tkinter-search-box/
+#https://www.youtube.com/watch?v=hkuT5swd9jI
